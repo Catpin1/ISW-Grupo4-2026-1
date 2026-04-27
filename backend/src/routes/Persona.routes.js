8b9fbf7479b4e976248c8cff21f27d325b45cadc
Catherine Pinares <catpinares@gmail.com>
2026-04-27 17:18:17 -0400

    Feat: se agregan los endpoints de Persona

---
+++ backend/src/routes/Persona.routes.js
@@ -0,0 +1,25 @@
+"use strict";
+
+import { Router } from "express";
+import { authenticateJwt } from "../middleware/authentication.mw.js";
+import { authorizeRoles } from "../middleware/authorization.mw.js";
+import { uploadFields } from "../middleware/upload.mw.js";
+
+import {
+    getpersona,
+    createpersona,
+    updatepersona,
+    deletepersona,  
+} from "../controllers/Persona.controller.js";
+
+const router = Router();
+
+router
+
+.get("/", authorizeRoles("admin"), getPersonas)
+.get("/detail/", authorizeRoles("admin"), getpersona)
+.post("/", uploadFields, authorizeRoles("admin"),createpersona)
+.patch("/detail/", uploadFields, authorizeRoles("admin"), updatepersona)
+.delete("/detail", authorizeRoles("admin"), deletepersona);
+
+export default router;
