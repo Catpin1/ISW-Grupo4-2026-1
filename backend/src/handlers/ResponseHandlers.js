739ff77cb5c4200881b782f9cba3f125e3c44eb4
Bruno Alfonso Castillo Bozzo <104660101+BrunoCastilloB@users.noreply.github.com>
2026-04-26 23:53:20 -0400

    Update ResponseHandlers.js
    Agregar ErrorClient y ErrorServer

--- backend/src/handlers/ResponseHandlers.js
+++ backend/src/handlers/ResponseHandlers.js
@@ -6,10 +6,18 @@ export const sendSuccess = (res, data, message = 'Operación exitosa', statusCod
   });
 };
 
-export const sendError = (res, error, statusCode = 500) => {
+export const sendErrorServer = (res, error, statusCode = 500) => {
   return res.status(statusCode).json({
     success: false,
     error: error.message || 'Error interno del servidor',
+    // Opcional: code: 'AUTH_001' para errores específicos
+  });
+};
+
+export const sendErrorClient = (res, error, statusCode = 400) => {
+  return res.status(statusCode).json({
+    success: false,
+    error: error.message || 'Error interno de usuario,
     // Opcional: code: 'AUTH_001' para errores específicos
   });
 };
