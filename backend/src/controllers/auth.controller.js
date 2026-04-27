b1ec8f0cbea3c5c1ed5f163f55b08a7ca8df1a4a
Bruno Castillo Bozzo <bruno.castillo1801@alumnos.ubiobio.cl>
2026-04-27 00:02:46 -0400

    creacion auth controller y passport auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,68 @@
 "use strict";
 
-import { loginService, registerService } from "../services/auth.service.js";
-import { authValidation, registerValidation } from "../services/auth.validation.js"
+import { authService, register } from "../services/auth.service.js";
+import { loginSchema, registerSchema } from "../validations/auth.val.js";
+import { sendErrorClient,
+         sendErrorServer,
+         sendSuccess,
+} from "../handlers/ResponseHandlers.js";
+
+export async function login(req, res){
+    try{
+        const { body } = req;
+        const { error } = loginSchema.validate(body);
+
+        if(error){
+            return sendErrorClient(res, 400, "Datos entrada invalidos", error.message);
+        }
+        const [result, errorLogin] = await authService(body);
+
+        if (errorLogin){
+            return sendErrorClient(res, 401, "Error autenticacion", errorLogin);
+        }
+
+        const { user, accessToken } = result;
+
+        res.cookie("jwt", accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 24*60*60*1000, //24 horas
+        });
+
+        sendSuccess(res, 200, "Inicio de sesion correcto", {user});
+    }catch (error){
+        sendErrorServer(res, 500, "Error interno de server", error.message);
+    }
+}
+
+export async function register(req, res) {
+    try{
+        const { body } = req;
+        const { error } = registerSchema.validate(body);
+        if (error) {
+            return sendErrorClient(res, 400, "Datos entrada invalidos", error.message);
+        }
+
+        const [user, errorRegister ] = await register(body);
+        if (errorRegister) {
+            return sendErrorClient(res, 400, "Error al registrar usuario", errorRegister);
+        }
+        sendSuccess(res, 201, "Usuario Registrado", user);
+    }catch(error){
+        sendErrorServer(res, 500, "Error interno de server", error.message);
+    }
+}
+export async function logout(req, res) {
+    try {
+    res.clearCookie("jwt", {
+        httpOnly:true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite:"strict",
+    });
+    sendSuccess(res, 200, "sesion cerrada");
+    }catch(error){
+        sendErrorServer(res, 500, "Error interno de server", error.message);
+    }
+    
+}
