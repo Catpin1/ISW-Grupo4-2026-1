2ff57a3447636e2b86a488157864bb4e6ecb5b38
Catherine Pinares <catpinares@gmail.com>
2026-05-18 13:45:16 -0400

    ADd: Se añaden rutas de entidad compra

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,6 +3,10 @@
 import { Router } from "express";
 import personaRoutes from "./Persona.routes.js";
 import authRoutes from "./auth.routes.js";
+import compraRoutes from "./Compra.routes.js";
+import claseRoutes from "./Clase.routes.js";
+import evaluacionPracticaRoutes from "./EvaluacionPractica.routes.js";
+import evaluacionTeoricaRoutes from "./EvaluacionTeorica.routes.js";
 
 const router = Router();
 
@@ -11,6 +15,10 @@ router.get("/", (req, res) => {
 });
 
 router.use("/auth", authRoutes);
+router.use("/compras", compraRoutes);
 router.use("/persona", personaRoutes);
+router.use("/clases", claseRoutes);
+router.use("/evaluaciones-practicas", evaluacionPracticaRoutes);
+router.use("/evaluaciones-teoricas", evaluacionTeoricaRoutes);
 
 export default router;
